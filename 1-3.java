785a2531057ddb6f103b6de30af5d94cb399e64d
matsmy <101180563+matsmy@users.noreply.github.com>
2022-03-22 10:27:23 +0000

    Initial commit

---
+++ 1-3.java
@@ -0,0 +1,11 @@
+import java.lang.Math;
+class Main
+  {
+    public static void main(String[] args)
+    {
+      double a = 2;
+      double b = 5;
+      double c = Math.pow(a, b);
+      System.out.println("Wynik potegowania 2^5 = "+c);
+    }  
+  }
